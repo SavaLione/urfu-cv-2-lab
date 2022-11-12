d119d133c8a42a81361789d8b312fc06e9c203ad
Saveliy (SavaLione) Pototskiy <monologuesplus@gmail.com>
2022-11-12 10:55:12 +0500

    Pre texture

--- src/core/lyssa.cpp
+++ src/core/lyssa.cpp
@@ -15,6 +15,12 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+// OpenCV
+#include <opencv2/opencv.hpp>
+// std
+#include <exception>
+#include <stdexcept>
+#include <string>
 
 // helper to check and display for shader compiler errors
 bool check_shader_compile_status(GLuint obj)
@@ -48,6 +54,78 @@ bool check_program_link_status(GLuint obj)
 		return false;
 	}
 	return true;
+}
+
+void bind_cvmat_to_gl_texture(cv::Mat &image_mat, GLuint &texture)
+{
+	if(image_mat.empty())
+	{
+		throw std::runtime_error("Can't load image from: cv::Mat &image_mat");
+	}
+	else
+	{
+		//glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+		glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+		glGenTextures(1, &texture);
+
+		// Error check
+		GLenum error_gen = glGetError();
+		if(error_gen != GL_NO_ERROR)
+		{
+			std::string _error = "OpenGL error after generation textures: ";
+			_error += std::to_string(error_gen);
+			throw std::runtime_error(_error);
+		}
+
+		glBindTexture(GL_TEXTURE_2D, texture);
+
+		GLenum error_bind = glGetError();
+		if(error_bind != GL_NO_ERROR)
+		{
+			std::string _error = "OpenGL error after bind textures: ";
+			_error += std::to_string(error_bind);
+			throw std::runtime_error(_error);
+		}
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		// Set texture clamping method
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+		// uv are not in [0,1] for some models, they assume uvs are repeated.
+		// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+		cv::cvtColor(image_mat, image_mat, cv::COLOR_RGB2BGR);
+
+		glTexImage2D(
+			GL_TEXTURE_2D,	  // Type of texture
+			0,				  // Pyramid level (for mip-mapping) - 0 is the top level
+			GL_RGB,			  // Internal colour format to convert to
+			image_mat.cols,	  // Image width  i.e. 640 for Kinect in standard mode
+			image_mat.rows,	  // Image height i.e. 480 for Kinect in standard mode
+			0,				  // Border width in pixels (can either be 1 or 0)
+			GL_RGB,			  // Input image format (i.e. GL_RGB, GL_RGBA, GL_BGR etc.)
+			GL_UNSIGNED_BYTE, // Image data type
+			image_mat.ptr()); // The actual image data itself
+
+		GLenum error_image_2d = glGetError();
+		if(error_image_2d != GL_NO_ERROR)
+		{
+			std::string _error = "OpenGL error after define two-dimensional texture image: ";
+			_error += std::to_string(error_image_2d);
+			throw std::runtime_error(_error);
+		}
+
+		//Random models from the
+		// internet come with random formats, and generating mipmaps may be
+		// non-trivial.
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
 }
 
 int main(int argc, char *argv[])
@@ -288,6 +366,7 @@ int main(int argc, char *argv[])
 
 	// texture handle
 	GLuint buffer_texture;
+	GLuint buffer_texture_with_mat;
 
 	// generate and bind the buffer texture
 	glGenTextures(1, &buffer_texture);
@@ -352,36 +431,48 @@ int main(int argc, char *argv[])
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// use the shader program
-		glUseProgram(shader_program);
+		// // use the shader program
+		// glUseProgram(shader_program);
 
-		// calculate ViewProjection matrix
-		glm::mat4 Projection = glm::perspective(90.0f, 4.0f / 3.0f, 0.1f, 100.f);
+		// // calculate ViewProjection matrix
+		// glm::mat4 Projection = glm::perspective(90.0f, 4.0f / 3.0f, 0.1f, 100.f);
 
-		// translate the world/view position
-		glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -5.0f));
+		// // translate the world/view position
+		// glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -5.0f));
 
-		// make the camera rotate around the origin
-		View = glm::rotate(View, 90.0f * t, glm::vec3(1.0f, 1.0f, 1.0f));
+		// // make the camera rotate around the origin
+		// View = glm::rotate(View, 90.0f * t, glm::vec3(1.0f, 1.0f, 1.0f));
 
-		glm::mat4 ViewProjection = Projection * View;
+		// glm::mat4 ViewProjection = Projection * View;
 
-		// bind texture to texture unit 0
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_BUFFER, buffer_texture);
+		// // bind texture to texture unit 0
+		// glActiveTexture(GL_TEXTURE0);
+		// glBindTexture(GL_TEXTURE_BUFFER, buffer_texture);
 
-		// set the matrix uniform
-		glUniformMatrix4fv(ViewProjection_location, 1, GL_FALSE, glm::value_ptr(ViewProjection));
+		// // set the matrix uniform
+		// glUniformMatrix4fv(ViewProjection_location, 1, GL_FALSE, glm::value_ptr(ViewProjection));
 
-		// set texture uniform
-		glUniform1i(offset_texture_location, 0);
+		// // set texture uniform
+		// glUniform1i(offset_texture_location, 0);
 
-		// bind the vao
-		glBindVertexArray(vao);
+		// // bind the vao
+		// glBindVertexArray(vao);
 
-		// draw
-		// the additional parameter indicates how many instances to render
-		glDrawElementsInstanced(GL_TRIANGLES, 6 * 6, GL_UNSIGNED_INT, 0, 8);
+		// // draw
+		// // the additional parameter indicates how many instances to render
+		// glDrawElementsInstanced(GL_TRIANGLES, 6 * 6, GL_UNSIGNED_INT, 0, 8);
+
+		// Texture
+		try
+		{
+			cv::Mat image = cv::imread("test.png");
+
+			bind_cvmat_to_gl_texture(image, buffer_texture_with_mat);
+		}
+		catch(std::exception &e)
+		{
+			spdlog::error("Error: {}", e.what());
+		}
 
 		// check for errors
 		GLenum error = glGetError();
@@ -401,6 +492,7 @@ int main(int argc, char *argv[])
 	glDeleteBuffers(1, &tbo);
 
 	glDeleteTextures(1, &buffer_texture);
+	glDeleteTextures(1, &buffer_texture_with_mat);
 
 	glDetachShader(shader_program, vertex_shader);
 	glDetachShader(shader_program, fragment_shader);
