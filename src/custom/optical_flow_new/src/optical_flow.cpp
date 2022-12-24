bfc2105ad000d7c9b13c79b07425af8f7d4722a6
Saveliy (SavaLione) Pototskiy <monologuesplus@gmail.com>
2022-12-24 12:52:25 +0500

    works

--- src/custom/optical_flow_new/src/optical_flow.cpp
+++ src/custom/optical_flow_new/src/optical_flow.cpp
@@ -1,17 +1,19 @@
 #include "optical_flow.h"
 
+#include <algorithm>
 #include <cstddef>
 #include <exception>
 #include <opencv2/core.hpp>
+#include <opencv2/core/matx.hpp>
 #include <opencv2/core/types.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
+#include <opencv2/video/tracking.hpp>
 #include <random>
 #include <spdlog/spdlog.h>
 #include <vector>
-
 
 bool BOOL_EXIT = false;
 
@@ -30,6 +32,12 @@ bool is_in_circle(point x1, point x2, int r)
 	else
 		return false;
 }
+
+// float my_max(float const& x, float const& y)
+// {
+// 	if(x + y)
+
+// }
 
 int main()
 {
@@ -113,7 +121,10 @@ int main()
 	// objects.push_back({120, 120});
 	// objects.push_back({20, 98});
 	// objects.push_back({300, 300});
-	for(std::size_t i = 0; i < 12; i++)
+
+	int objects_size = 16;
+
+	for(std::size_t i = 0; i < objects_size; i++)
 	{
 		point random_circle;
 		random_circle.x = dist_x(my_rng);
@@ -142,17 +153,18 @@ int main()
 			}
 
 			if(time > 60)
+			// if(time > 0)
 			{
 				time = 0;
 				mask = cv::Mat::zeros(old_frame.size(), old_frame.type());
 			}
 
-			// if(p0.size() >= 16)
+			// if(p0.size() >= 2)
 			// {
 			// 	p0.clear();
 			// }
 
-			// if(p1.size() >= 16)
+			// if(p1.size() >= 2)
 			// {
 			// 	p1.clear();
 			// }
@@ -174,59 +186,123 @@ int main()
 			std::vector<uchar> status;
 			std::vector<float> err;
 			cv::TermCriteria criteria = cv::TermCriteria((cv::TermCriteria::COUNT) + (cv::TermCriteria::EPS), 10, 0.03);
-			cv::calcOpticalFlowPyrLK(old_gray, frame_gray, p0, p1, status, err, cv::Size(15, 15), 2, criteria);
+			// cv::calcOpticalFlowPyrLK(old_gray, frame_gray, p0, p1, status, err, cv::Size(15, 15), 2, criteria);
+			// cv::calcOpticalFlowPyrLK(old_gray, frame_gray, p0, p1, status, err, cv::Size(15, 15), 2, criteria);
+			cv::Mat flow;
+			// cv::FarnebackOpticalFlow(old_gray, frame_gray, flow, status, err, cv::Size(15, 15), 2, criteria);
+			cv::calcOpticalFlowFarneback(old_gray, frame_gray, flow, 0.5, 5, 5, 3, 7, 1.5, cv::OPTFLOW_FARNEBACK_GAUSSIAN);
 
-			std::vector<cv::Point2f> good_new;
-			// Do we have any good points?
-			bool fl = false;
-			for(uint i = 0; i < p0.size(); i++)
+			int step = 20;
+
+			// for(std::size_t y = 0; y < flow.rows; y++)
+			for(std::size_t y = 0; y < flow.rows; y += step)
 			{
-				// Select good points
-				if(status[i] == 1)
+				// for(std::size_t x = 0; x < flow.cols; x++)
+				for(std::size_t x = 0; x < flow.cols; x += step)
 				{
-					good_new.push_back(p1[i]);
-					// draw the tracks
-					line(mask, p1[i], p0[i], colors[i], 2);
-					circle(frame, p1[i], 5, colors[i], -1);
+					// cv::line(frame, {(int)x, (int)y}, cv::Vec2i(x, y), cv::Scalar(255, 255, 255));
+					// cv::line(frame, {(int)x, (int)y}, cv::Vec2i(x + flow.depth(), y + flow.depth()), cv::Scalar(255, 255, 255));
 
-					fl = true;
+					// cv::line(frame, {(int)y, (int)x}, cv::Vec2i(x + flow.at<cv::Vec2f>(y, x)[1], y + flow.at<cv::Vec2f>(y, x)[0]), cv::Scalar(255, 255, 255));
+
+					// if(flow)
+					// {
+
+					// }
+					// else
+					// {
+
+					// }
+
+					// cv::line(frame, {(int)x, (int)y}, {(int)x, (int)y}, cv::Scalar(255, 255, 255));
+					// cv::line(frame, {(int)x, (int)y}, cv::Vec2i(x + flow.at<cv::Vec2f>(x, y)[0], y + flow.at<cv::Vec2f>(x, y)[0]), cv::Scalar(255, 255, 255));
+					// cv::line(frame, {(int)x, (int)y}, cv::Vec2i(x + 10, y + 10), cv::Scalar(255, 255, 255));
+
+					// if(flow.at<cv::Vec2f>(x, y)[0] == 0 && flow.at<cv::Vec2f>(x, y)[1] == 0)
+					// {
+
+					// }
+					// else
+					// {
+					// 	cv::line(frame, {(int)x, (int)y}, cv::Vec2i(x + 10, y + 10), cv::Scalar(255, 255, 255));
+					// }
+
+					int v_x = x + flow.at<cv::Vec2f>(y, x)[0];
+					int v_y = y + flow.at<cv::Vec2f>(y, x)[1];
+
+					//if(v_x > 640)
+					//	v_x = 640;
+
+					v_x = std::min(v_x, 639);
+					v_y = std::min(v_y, 639);
+
+					v_x = std::max(v_x, 0);
+					v_y = std::max(v_y, 0);
+
+					// cv::line(
+					// 	frame,
+					// 	{(int)x, (int)y},
+					// 	cv::Vec2i(x + flow.at<cv::Vec2f>(x, y)[0], y + flow.at<cv::Vec2f>(x, y)[0]),
+					// 	cv::Scalar(255, 255, 255));
+					cv::line(
+						frame,
+						{(int)x, (int)y},
+						cv::Vec2i(v_x, v_y),
+						cv::Scalar(255, 255, 255));
 				}
 			}
 
-			if(!fl)
-			{
-				throw std::runtime_error("We do not have any good points");
-			}
+			// std::vector<cv::Point2f> good_new;
+			// // Do we have any good points?
+			// bool fl = false;
+			// for(uint i = 0; i < p0.size(); i++)
+			// {
+			// 	// Select good points
+			// 	if(status[i] == 1)
+			// 	{
+			// 		good_new.push_back(p1[i]);
+			// 		// draw the tracks
+			// 		line(mask, p1[i], p0[i], colors[i], 2);
+			// 		circle(frame, p1[i], 5, colors[i], -1);
+
+			// 		fl = true;
+			// 	}
+			// }
+
+			// if(!fl)
+			// {
+			// 	throw std::runtime_error("We do not have any good points");
+			// }
 
 			cv::Mat img;
 			add(frame, mask, img);
 
-			// Objects
-			for(std::size_t i = 0; i < objects.size(); i++)
-			{
-				for(std::size_t z = 0; z < good_new.size(); z++)
-				{
-					point x1;
-					point x2 = {(int)good_new[z].x, (int)good_new[z].y};
-					if(is_in_circle(objects[i], x2, circle_radius))
-					{
-						spdlog::info("point x: {} y: {} - is in circle", objects[i].x, objects[i].y);
-						while(is_in_circle(objects[i], x2, circle_radius))
-						{
-							objects[i].x++;
-							objects[i].y++;
-						}
-					}
-				}
-			}
+			// // Objects
+			// for(std::size_t i = 0; i < objects.size(); i++)
+			// {
+			// 	for(std::size_t z = 0; z < good_new.size(); z++)
+			// 	{
+			// 		point x1;
+			// 		point x2 = {(int)good_new[z].x, (int)good_new[z].y};
+			// 		if(is_in_circle(objects[i], x2, circle_radius))
+			// 		{
+			// 			spdlog::info("point x: {} y: {} - is in circle", objects[i].x, objects[i].y);
+			// 			while(is_in_circle(objects[i], x2, circle_radius))
+			// 			{
+			// 				objects[i].x++;
+			// 				objects[i].y++;
+			// 			}
+			// 		}
+			// 	}
+			// }
 
-			// Objects
-			for(std::size_t i = 0; i < objects.size(); i++)
-			{
-				//Color of the circle
-				cv::Scalar line_color(255, 255, 255);
-				cv::circle(frame, {objects[i].x, objects[i].y}, circle_radius, line_color, 4, 8);
-			}
+			// // Objects
+			// for(std::size_t i = 0; i < objects.size(); i++)
+			// {
+			// 	//Color of the circle
+			// 	cv::Scalar line_color(255, 255, 255);
+			// 	cv::circle(frame, {objects[i].x, objects[i].y}, circle_radius, line_color, 4, 8);
+			// }
 
 			/* We are showing the result */
 			cv::imshow("Window 1", frame);
@@ -241,7 +317,7 @@ int main()
 
 			// Now update the previous frame and previous points
 			old_gray = frame_gray.clone();
-			p0		 = good_new;
+			// p0		 = good_new;
 		}
 		catch(std::exception const &e)
 		{
